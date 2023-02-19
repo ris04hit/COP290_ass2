ab45dd63bd0cf8e56023a6045d0bf9143377fc39
ris04hit <ris04hit@gmail.com>
2023-02-19 21:40:43 +0000

    Dummy commit

--- 2021CS10547_2021CS10121_2021CS50593/part2/src/hm.c
+++ 2021CS10547_2021CS10121_2021CS50593/part2/src/hm.c
@@ -28,18 +28,25 @@ int hashmap_create(struct hashmap_s *const out_hashmap)   // Initialize a hashma
 int hash_func(const char* key){
   int p = 257;
   int m = 4093;
-  char *k = (char*) key;
+  char k = key[0];
+  int i = 0;
   int hash_code = 0;
-  while (*k != '\0'){
-    int asc = (int) *k;
+  while (i < strlen(key)){
+    // printf("%d\n", k);
+    // puts(k);
+    // printf("comp\n");
+    int asc = (int) k;
+    if (asc < 0){asc = -asc;}
     hash_code = (p*hash_code + asc)%m;
-    k++;
+    i++;
+    k = key[i];
   }
   return hash_code;
 }
 
 int hashmap_put(struct hashmap_s *const hashmap, const char* key, void* data)   // Set value of the key as data in hashmap. You can use any method to resolve conflicts. Also write your own hashing function
 {
+  // printf("Call by put %s\n", key);
   int hash_code = hash_func(key);
   struct hashmap_element_s* elem = (struct hashmap_element_s*)malloc(sizeof(struct hashmap_element_s));
   char* k = (char*)malloc((strlen(key)+1)*sizeof(char));
@@ -71,8 +78,11 @@ int hashmap_put(struct hashmap_s *const hashmap, const char* key, void* data)   
 
 void* hashmap_get(struct hashmap_s *const hashmap, const char* key)    // Fetch value of a key from hashmap
 {
+  printf("Call by get %s\n", key);
   int hash_code = hash_func(key);
+  // printf("%d\n",hash_code);
   if (hashmap->table[hash_code] == NULL){return NULL;}
+  // printf("b\n");
   struct listentry* Node = hashmap->table[hash_code]->head;
   while (Node != NULL){
     if (strcmp(((struct hashmap_element_s *)(Node->data))->key,key)==0){
@@ -100,15 +110,22 @@ void hashmap_iterator(struct hashmap_s* const hashmap, int (*f)(struct hashmap_e
 
 int acquire_bucket(struct hashmap_s *const hashmap, const char* key)   // Acquire lock on a hashmap slot
 {
+    // printf("Call by ab\n");// %s\n", key);
+    // puts(key);
+    // printf("done %ld\n", strlen(key));
   int hash_code = hash_func(key);
+  // printf("acq buck hash code %d\n",hash_code);
   if (hashmap->lk[hash_code] == NULL){
     hashmap->lk[hash_code] = lock_new();
+    // printf("New lock\n");
   }
   lock_acquire(hashmap->lk[hash_code]);
+  // printf("Lock acquired\n");
 }
 
 int release_bucket(struct hashmap_s *const hashmap, const char* key)   // Release acquired lock
 {
+    // printf("Call by rb %s\n", key);
   int  hash_code = hash_func(key);
   if (hashmap->lk[hash_code] != NULL){
     lock_release(hashmap->lk[hash_code]);
